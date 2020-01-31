7247081a0b33263b983d343ff0ff0367dcecd710
romcooo.desktop2 <stubna@gmail.com>
2020-01-31 14:52:02 +0100

    DONE (updated Scanner to use InputStreamReader and BufferedReader )
    Signed-off-by: romcooo.desktop2 <stubna@gmail.com>

--- googleChallenges/src/main/java/com/romco/pizzaChallenge/InputParser.java
+++ googleChallenges/src/main/java/com/romco/pizzaChallenge/InputParser.java
@@ -1,11 +1,13 @@
 package com.romco.pizzaChallenge;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 class InputParser {
     public static Requirement parse() {
         Requirement requirement = new Requirement();
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
         requirement.setMaxSlices(scanner.nextInt());
         requirement.setMaxNumberOfPizzas(scanner.nextInt());
         scanner.nextLine();
