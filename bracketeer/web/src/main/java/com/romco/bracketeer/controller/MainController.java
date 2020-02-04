3fa589a0347097107eda1e79b8c527fccec5772e
romcooo.desktop2 <stubna@gmail.com>
2020-02-05 00:08:02 +0100

    some idk

--- bracketeer/web/src/main/java/com/romco/bracketeer/controller/MainController.java
+++ bracketeer/web/src/main/java/com/romco/bracketeer/controller/MainController.java
@@ -1,9 +1,11 @@
 package com.romco.bracketeer.controller;
 
 import com.romco.bracketeer.service.MainService;
+import com.romco.bracketeer.util.ViewNames;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
@@ -16,10 +18,10 @@ public class MainController {
         this.service = service;
     }
 
-    // == request methods
-//    @GetMapping("home")
-//    public String welcome() {
-//        log.info("user arrived");
-//        return ViewNames.HOME;
-//    }
+     // == request methods
+    @GetMapping("new")
+    public String newTournament() {
+        log.info("in newTournament");
+        return ViewNames.HOME;
+    }
 }
