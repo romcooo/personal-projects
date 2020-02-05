0d668db7e9fe0bea3e714b3dc6e64f62ba5d37a2
Roman.Stubna <roman.stubna@embedit.cz>
2020-02-05 12:31:38 +0100

    started work on model

---
+++ bracketeer/web/src/main/java/com/romco/bracketeer/model/Team.java
@@ -0,0 +1,19 @@
+package com.romco.bracketeer.model;
+
+import java.util.List;
+
+public class Team<T> extends Participant {
+    private List<T> members;
+    
+    public Team() {
+    }
+    
+    public boolean add(T member) {
+        if (!members.contains(member)) {
+            members.add(member);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
