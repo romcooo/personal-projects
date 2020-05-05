c924cf0be72e7b2330d00860ce3469fb46b69827
romcooo.desktop2 <stubna@gmail.com>
2020-05-05 10:34:52 +0200

    pweqew

--- bracketeer/user-management/src/main/java/com/romco/bracketeer/persistence/dao/UserDao.kt
+++ bracketeer/user-management/src/main/java/com/romco/bracketeer/persistence/dao/UserDao.kt
@@ -1,5 +1,21 @@
 package com.romco.bracketeer.persistence.dao
 
+import javax.sql.DataSource
+
 interface UserDao {
-    fun create()
+    fun setDataSource(dataSource: DataSource?)
+
+    // retrieves all
+    fun <T> retrieveAll(): Collection<T?>?
+
+    // creates and returns id of the newly created record. In case of failure, returns -1.
+    fun <T> create(t: T?): Long
+
+    // updates by id, returns true if success.
+    fun <T> update(t: T?): Boolean
+
+    // deletes by id, returns true if success.
+    fun <T> delete(t: T?): Boolean
+
+    fun cleanup()
 }
---
+++ bracketeer/user-management/src/main/java/com/romco/bracketeer/persistence/daoimpl/UserDaoImpl.kt
@@ -0,0 +1,5 @@
+package com.romco.bracketeer.persistence.daoimpl
+
+class UserDaoImpl {
+
+}
